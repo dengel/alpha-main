b10b95ffb008ac08f592ce1c0d09fd46a6252c34
Daniel Engel <dengel@bandwidth.com>
2014-02-25 13:59:02 -0500

    Checkpoint

--- go-mgr.rb
+++ go-mgr.rb
@@ -1,18 +1,33 @@
 #!/usr/bin/env ruby
 #
+
+# System
+require 'optparse'
+
+# Local
 $LOAD_PATH << './lib'
 require 'NextProject.rb'
 
+OptionParser.new do |o|
+  o.on('-s') { |b| $setup = b }
+  o.on('-g PROJECT') { |project| $git = project }
+  o.on('-l LINKS') { |links| $links = links }
+  o.on('-f FILENAME') { |filename| $filename = filename }
+  o.on('-h') { puts o; exit }
+  o.parse!
+end
+
 proj = NextProject.new()
 
-next_name = proj.next_project()
-
-puts "*** Project #{next_name}" 
-
-proj.set_value(next_name, proj.build_project(next_name));
-
-proj.update_file()
-
-proj.create_hierarchy(next_name)
-
-proj.create_control_info(next_name)
+if ( $setup )
+	next_name = proj.next_project()
+	puts "*** Project #{next_name}" 
+	proj.set_value(next_name, proj.build_project(next_name));
+	proj.update_file()
+	proj.create_hierarchy(next_name)
+	proj.create_control_info(next_name)
+elsif ( $git )
+	proj.create_scm($git)
+elsif ( $links )
+	proj.create_links($links)
+end
--- lib/NextProject.rb
+++ lib/NextProject.rb
@@ -5,95 +5,130 @@ require 'yaml'
 
 class NextProject
 
-  #BASEDIR = "~/Dropbox/go"
-  BASEDIR = "/tmp/go"
+    #BASEDIR = "~/Dropbox/go"
+    BASEDIR = "/tmp/go"
 
-	# Load project config file
-	def initialize()
-		@config_file="projects.yml"
-		@config = YAML.load_file(@config_file)
-	end
-
-	# find available project
-	def next_project()
-		config = @config
-		ret = "none"
-		config.each do |key, array|
-			ret=key
-			break if (config[key] == false)
-		end
-		return ret
-	end
-
-	# set configuration option
-	def set_value(key, val)
-		@config[key] = val
-	end
-
-	def update_file()
-		File.open(@config_file, 'w') {|f| f.write @config.to_yaml }
-	end
-
-  # Create the project definition TODO: Use ERB
-  def build_project(go)
-    content = {
-      "active" => true,
-      "path" => BASEDIR + "/#{go}"
-    }
-    return content
-  end
-
-  def create_hierarchy(go)
-    go_path= BASEDIR + "/" + go
-    path_list = Array[ "#{go_path}",
-      "#{go_path}/documents",
-      "#{go_path}/invoices",
-      "#{go_path}/source",
-      "#{go_path}/meta"
-    ]
-    path_list.each do |p|
-      puts "> Creating: #{p}"
-      FileUtils.mkdir_p(p) unless File.exists?(p)
+    # Load project config file
+    def initialize()
+        @config_file="projects.yml"
+        @config = YAML.load_file(@config_file)
     end
-  end
 
-  def create_control_info(go)
-    path= BASEDIR + "/" + go + "/meta/client.yml"
-    puts "> Creating: #{path}"
-    records = {
-      "name" => "Acme",
-      "project" => "TBD",
-      "rate" => "TBD",
-      "codename" => "TBD",
-      "realname" => "TBD",
-      "status" => "NEW",
-    }
-    File.open(path, 'w') {|f| f.write records.to_yaml }
+    # find available project
+    def next_project()
+        config = @config
+        ret = "none"
+        config.each do |key, array|
+            ret=key
+            break if (config[key] == false)
+        end
+        return ret
+    end
 
-    path= BASEDIR + "/" + go + "/meta/control.yml"
-    puts "> Creating: #{path}"
-    records = {
-      "status" => "NEW",
-      "type" => "TBD",
-      "penflip" => "TBD",
-      "github" => [ false ],
-    }
-    File.open(path, 'w') {|f| f.write records.to_yaml }
+    # set configuration option
+    def set_value(key, val)
+        @config[key] = val
+    end
 
-    path= BASEDIR + "/" + go + "/meta/links.yml"
-    puts "> Creating: #{path}"
-    records = {
-      "category1" => { 
-        "link1" => "http://localhost/1",
-        "link2" => "http://localhost/2",
-       },
-      "category2" => { 
-        "link3" => "http://localhost/3",
-        "link4" => "http://localhost/4",
-       },
-    }
-    File.open(path, 'w') {|f| f.write records.to_yaml }
+    # erite configuration file
+    def update_file()
+        File.open(@config_file, 'w') {|f| f.write @config.to_yaml }
+    end
 
-  end
+    # Create the project definition TODO: Use ERB
+    def build_project(go)
+        content = {
+            "active" => true,
+            "path" => BASEDIR + "/#{go}"
+        }
+        return content
+    end
 
+    def create_hierarchy(go)
+        go_path= BASEDIR + "/" + go
+        path_list = Array[ "#{go_path}",
+            "#{go_path}/documents",
+            "#{go_path}/invoices",
+            "#{go_path}/source",
+            "#{go_path}/meta"
+        ]
+        path_list.each do |p|
+            puts "> Creating: #{p}"
+            FileUtils.mkdir_p(p) unless File.exists?(p)
+        end
+    end
+
+    def create_control_info(go)
+        path= BASEDIR + "/" + go + "/meta/client.yml"
+        puts "> Creating: #{path}"
+        records = {
+            "name" => "Acme",
+            "project" => "TBD",
+            "rate" => "TBD",
+            "codename" => "TBD",
+            "realname" => "TBD",
+            "status" => "NEW",
+        }
+
+        File.open(path, 'w') {|f| f.write records.to_yaml }
+
+        path= BASEDIR + "/" + go + "/meta/control.yml"
+        puts "> Creating: #{path}"
+        records = {
+            "status" => "NEW",
+            "type" => "TBD",
+            "penflip" => "TBD",
+            "gitrepo" => [ false ],
+        }
+
+        File.open(path, 'w') {|f| f.write records.to_yaml }
+
+        path= BASEDIR + "/" + go + "/meta/links.yml"
+        puts "> Creating: #{path}"
+        records = {
+            "category1" => { 
+                "link1" => "http://localhost/1",
+                "link2" => "http://localhost/2",
+            },
+            "category2" => { 
+                "link3" => "http://localhost/3",
+                "link4" => "http://localhost/4",
+            },
+        }
+
+        File.open(path, 'w') {|f| f.write records.to_yaml }
+
+    end
+
+    def create_scm(go)
+        path = BASEDIR + "/" + go + "/meta/control.yml"
+        proj = YAML.load_file(path)
+        git = proj["gitrepo"]
+
+        return if git.nil?
+
+        git.each do |e|
+        repo = e.gsub(/^.*\//, '').gsub(/.git/, '')
+        code = BASEDIR + "/" + go + "/source/" + repo
+        puts "Repo: #{e}"
+            if File.directory?(code)
+                puts "#{code} already exists"
+            else
+                # TODO: Use gem
+                `git clone #{e} #{code}`
+            end
+        end
+    end
+
+    def create_links(go)
+        path = BASEDIR + "/" + go + "/meta/links.yml"
+        proj = YAML.load_file(path)
+        proj.each do |p|
+            puts "<h3>#{p[0]}</h3>"
+	    p[1].each do |u, l|
+                puts "<a href=\"#{u}\" class=\"btn btn-block btn-primary\">#{l}</a>"
+            end
+        end
+    end
+    
 end
